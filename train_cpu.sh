65e8251d074c75daf25d3e735d6cf19106045469
Akhitha Manjitha <asmanjitha@gmail.com>
2025-11-16 00:21:31 -0500

    removed n tasks per cpu

--- train_cpu.sh
+++ train_cpu.sh
@@ -7,9 +7,8 @@
 #SBATCH --account=kj373                 #  PI's UCID
 #SBATCH --mail-user asm277@njit.edu
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
+#SBATCH --nodes=5
 #SBATCH --ntasks=512
-#SBATCH --cpus-per-task=4               # adjust if you need fewer/more cores
 #SBATCH --time=72:00:00                 # max for standard/low QoS is 72h
 #SBATCH --mem=16G                       # adjust to your needs
 
