41da9f6f4cf1675f078e2e9e255ef0fd7f392ea4
Akhitha Manjitha <asmanjitha@gmail.com>
2025-11-16 15:48:09 -0500

    increase episodes to 10000

--- train_cpu.sh
+++ train_cpu.sh
@@ -7,7 +7,7 @@
 #SBATCH --account=kj373                 #  PI's UCID
 #SBATCH --mail-user asm277@njit.edu
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=8
 #SBATCH --time=72:00:00                 # max for standard/low QoS is 72h
 #SBATCH --mem-per-cpu=8000M                       
