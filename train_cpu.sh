d019e1fb5456cb2912ea02d5ca343549843cf252
Akhitha Manjitha <asmanjitha@gmail.com>
2025-11-19 13:59:14 -0500

    train_cpu.sh changed

--- train_cpu.sh
+++ train_cpu.sh
@@ -1,27 +1,18 @@
 #!/bin/bash -l
 #SBATCH --job-name=rl_train_asm
-#SBATCH --output=./hpc_logs/rl_train_asm.%j.out        # stdout goes here
-#SBATCH --error=rl_train_asm.%j.err         # stderr goes here
-#SBATCH --partition=general             # CPU partition
-#SBATCH --qos=standard                       # lowest priority
-#SBATCH --account=kj373                 #  PI's UCID
-#SBATCH --mail-user asm277@njit.edu
+#SBATCH --output=./hpc_logs/%x.%j.out
+#SBATCH --error=./hpc_logs/%x.%j.err
+#SBATCH --partition=general
+#SBATCH --qos=standard
+#SBATCH --account=kj373
+#SBATCH --mail-user=asm277@njit.edu
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=2
+#SBATCH --nodes=8
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=72:00:00                 # max for standard/low QoS is 72h
-#SBATCH --mem-per-cpu=8000M                       
+#SBATCH --time=72:00:00
+#SBATCH --mem-per-cpu=16000M
 
-# ---- Environment setup ----
-# Example: if you use modules + conda/venv, adapt to your setup
-
-module load bright python39  # or whichever python module HPC recommends
-
-# Activate venv:
+module load bright python39
 source /mmfs1/scratch/kj373/asm277/HIL-RL/python39/bin/activate
-
-# ---- Go to your project dir ----
 cd /mmfs1/scratch/kj373/asm277/HIL-RL/HIL-RL-Nav-Agent/NN
-
-# ---- Run your RL training ----
 python example_dqn.py
