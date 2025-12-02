0ac7012a67e972130476eaf3f5b7e63f5fd82af6
Akhitha Manjitha <asmanjitha@gmail.com>
2025-12-01 23:19:26 -0500

    checkout to feedbacks-foor-set-of-steps

--- NN/run_trained_dqn.py
+++ NN/run_trained_dqn.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     action_size = action_mapper.ACTION_SIZE
 
     agent = DQNAgent(state_size, action_size)
-    agent.load("weights/20251128-160442_20000_runs_weight_after1800_episodes.h5")
+    agent.load("weights/20251128-184508_20000_runs_weight_after500_episodes.h5")
     agent.epsilon = 0.0  
 
     env.activate_visuals(True)
